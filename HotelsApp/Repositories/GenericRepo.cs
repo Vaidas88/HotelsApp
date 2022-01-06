5a3c24010018bc7fad25f92e872f5bebac628088
Vaidas88 <v.butkus@outlook.com>
2022-01-06 21:22:06 +0200

    m

---
+++ HotelsApp/Repositories/GenericRepo.cs
@@ -0,0 +1,39 @@
+﻿using HotelsApp.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HotelsApp.Repositories
+{
+    public class GenericRepo<T> where T : class
+    {
+        internal DataContext _context;
+        internal DbSet<T> _dbSet;
+
+        public GenericRepo(DataContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public List<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
