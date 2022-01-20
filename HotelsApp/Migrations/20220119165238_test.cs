7e5576780f7c42ba1e66390771be7171b064c927
Vaidas88 <v.butkus@outlook.com>
2022-01-20 21:36:43 +0200

    last commit

---
+++ HotelsApp/Migrations/20220119165238_test.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HotelsApp.Migrations
+{
+    public partial class test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cleaners_Cities_CityId",
+                table: "Cleaners");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Hotels_Cities_CityId",
+                table: "Hotels");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rooms_Hotels_HotelId",
+                table: "Rooms");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "HotelId",
+                table: "Rooms",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CityId",
+                table: "Hotels",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CityId",
+                table: "Cleaners",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cleaners_Cities_CityId",
+                table: "Cleaners",
+                column: "CityId",
+                principalTable: "Cities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Hotels_Cities_CityId",
+                table: "Hotels",
+                column: "CityId",
+                principalTable: "Cities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rooms_Hotels_HotelId",
+                table: "Rooms",
+                column: "HotelId",
+                principalTable: "Hotels",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cleaners_Cities_CityId",
+                table: "Cleaners");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Hotels_Cities_CityId",
+                table: "Hotels");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rooms_Hotels_HotelId",
+                table: "Rooms");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "HotelId",
+                table: "Rooms",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CityId",
+                table: "Hotels",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CityId",
+                table: "Cleaners",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cleaners_Cities_CityId",
+                table: "Cleaners",
+                column: "CityId",
+                principalTable: "Cities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Hotels_Cities_CityId",
+                table: "Hotels",
+                column: "CityId",
+                principalTable: "Cities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rooms_Hotels_HotelId",
+                table: "Rooms",
+                column: "HotelId",
+                principalTable: "Hotels",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
